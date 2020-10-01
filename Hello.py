e2195d876e1d0cbb11b72f9d2d38ac4effefd975
anshmelo <56475119+anshmelo@users.noreply.github.com>
2020-10-01 21:01:17 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,1 +1,18 @@
 Print("Hello World")
+
+
+#Adding a feature which can determine the factorial of a input
+
+num = int(input("Enter a number: "))
+
+factorial = 1
+
+# check if the number is negative, positive or zero
+if num < 0:
+   print("Sorry, factorial not possible is negative numbers")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:
+   for i in range(1,num + 1):
+       factorial = factorial*i
+   print("The factorial of",num,"is",factorial)
