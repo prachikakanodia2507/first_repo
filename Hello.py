f630569c39557e56f3ced4a2719495868cadbdf3
akshi-ag <57820629+akshi-ag@users.noreply.github.com>
2020-10-02 01:16:26 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -64,5 +64,28 @@ print('The subtraction of {0} and {1} is {2}'.format(num1, num2, min))
 print('The multiplication of {0} and {1} is {2}'.format(num1, num2, mul))  
 # Display the division  
 print('The division of {0} and {1} is {2}'.format(num1, num2, div))  
-
+#Initialize array     
+arr = [5, 2, 8, 7, 1];     
+temp = 0;    
+     
+#Displaying elements of original array    
+print("Elements of original array: ");    
+for i in range(0, len(arr)):    
+    print(arr[i], end=" ");    
+     
+#Sort the array in ascending order    
+for i in range(0, len(arr)):    
+    for j in range(i+1, len(arr)):    
+        if(arr[i] > arr[j]):    
+            temp = arr[i];    
+            arr[i] = arr[j];    
+            arr[j] = temp;    
+     
+print();    
+     
+#Displaying elements of the array after sorting    
+    
+print("Elements of array sorted in ascending order: ");    
+for i in range(0, len(arr)):    
+    print(arr[i], end=" ");    
  
