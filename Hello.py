204d3b09292c2de7c85868818e3b00b840b5afe6
prachikakanodia2507 <73177603+prachikakanodia2507@users.noreply.github.com>
2020-10-21 21:22:07 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -89,3 +89,24 @@ print("Elements of array sorted in ascending order: ");
 for i in range(0, len(arr)):    
     print(arr[i], end=" ");    
  
+# Python program to find the largest number among the three input numbers
+
+# change the values of num1, num2 and num3
+# for a different result
+num1 = 10
+num2 = 14
+num3 = 12
+
+# uncomment following lines to take three numbers from user
+#num1 = float(input("Enter first number: "))
+#num2 = float(input("Enter second number: "))
+#num3 = float(input("Enter third number: "))
+
+if (num1 >= num2) and (num1 >= num3):
+   largest = num1
+elif (num2 >= num1) and (num2 >= num3):
+   largest = num2
+else:
+   largest = num3
+#la
+print("The largest number is", largest)
