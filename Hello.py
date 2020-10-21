beee9fee604bcd5364f4e549ca930e45c82420b1
prachikakanodia2507 <73177603+prachikakanodia2507@users.noreply.github.com>
2020-10-21 21:12:56 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -98,6 +98,15 @@ num1 = 10
 num2 = 14
 num3 = 12
 
+ 
+# Python program to find the largest number among the three input numbers
+
+# change the values of num1, num2 and num3
+# for a different result
+num1 = 10
+num2 = 14
+num3 = 12
+
 # uncomment following lines to take three numbers from user
 #num1 = float(input("Enter first number: "))
 #num2 = float(input("Enter second number: "))
